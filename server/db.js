a2afa3547588f7b2c035b7423a8af99a30c4a853
narang0619 <sinso5281532@gmail.com>
2025-12-03 11:55:53 +0900

    2025-12-03 개인 sns 프로젝트(최종)

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'test1234',
-    database: 'snsproject' // db 이름
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'your_mysql_password',
+    database: process.env.DB_NAME || 'snsproject'
 });
 
 
